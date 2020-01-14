2298877481106ebd644f407170f7229a6f06819d
Jeppe Feld <jafeld@gmail.com>
2020-01-14 11:31:07 +0100

    Added unit test to util function

---
+++ src/util/stripToInt.test.ts
@@ -0,0 +1,14 @@
+import { stripToInt } from "./stripToInt";
+
+
+describe('stripToInt', () => {
+	it('strips string to int', () => {
+		const input = stripToInt("foo123bar321");
+		expect(input).toEqual(123321);
+	});
+	
+	it('returns null if string doesnt contain a number', async () => {
+		const input = stripToInt("foo bar");
+		expect(input).toEqual(null);
+	});
+});
--- src/util/stripToInt.ts
+++ src/util/stripToInt.ts
@@ -1,4 +1,5 @@
 export const stripToInt = (input: string): number | null => {
   input = input.replace(/\D/g,'');
-  return parseInt(input);
+  const number = parseInt(input);
+  return number ? number : null;
 };
