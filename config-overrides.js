af45ed3054b95f64f86b01f19efa7871c2f2c686
MajaHelene <maja.helene.pedersen@nav.no>
2020-01-21 15:15:41 +0100

    Bruk uppercase env-variabler

--- config-overrides.js
+++ config-overrides.js
@@ -10,7 +10,7 @@ const devServerConfig = () => config => {
         ...config,
         proxy: {
             '/api': {
-                target: process.env.proxy || 'http://localhost:8080',
+                target: process.env.PROXY || 'http://localhost:8080',
                 secure: false,
                 changeOrigin: true,
                 pathRewrite: {
