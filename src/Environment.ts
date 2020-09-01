df3704f0d591bb1bacc3c015985749083ca9ca79
tuxbear <ole-andre.riga.johansen@nav.no>
2020-09-01 13:43:12 +0200

    Ny ingress for å nå dev miljøet fra egen maskin

--- src/Environment.ts
+++ src/Environment.ts
@@ -12,7 +12,7 @@ class Environment {
     get loginServiceUrl() {
         switch (this.environmentMode) {
             case EnvironmentType.PROD : return 'https://loginservice.nav.no/login?redirect=https://arbeidsgiver.nav.no/min-side-refusjon/';
-            case EnvironmentType.PREPROD : return 'https://loginservice-q.nav.no/login?redirect=https://arbeidsgiver-q.nav.no/min-side-refusjon/';
+            case EnvironmentType.PREPROD : return 'https://loginservice.dev.nav.no/login?redirect=https://arbeidsgiver.dev.nav.no/min-side-refusjon/';
             default : return 'http://localhost:8080/local/cookie-please?subject=12321&redirect=http://localhost:3000/min-side-refusjon/';
         }
     }
@@ -20,7 +20,7 @@ class Environment {
     get baseUrl() {
         switch (this.environmentMode) {
             case EnvironmentType.PROD : return 'https://arbeidsgiver.nav.no/min-side-refusjon';
-            case EnvironmentType.PREPROD : return 'https://arbeidsgiver-q.nav.no/min-side-refusjon';
+            case EnvironmentType.PREPROD : return 'https://arbeidsgiver.dev.nav.no/min-side-refusjon';
             default : return 'http://localhost:3000';
         }
     }
@@ -29,7 +29,7 @@ class Environment {
         if (window.location.hostname === 'localhost') {
             return EnvironmentType.LOCAL;
         }
-        if (window.location.hostname.indexOf('-q') > -1) {
+        if (window.location.hostname.indexOf('.dev.nav.no') > -1) {
             return EnvironmentType.PREPROD;
         }
         return EnvironmentType.PROD;
