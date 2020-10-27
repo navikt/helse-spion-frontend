080823e4878f72bc52f7b396e535e22df02c9fb0
Kent Fjellstad <kent.fjellstad@nav.no>
2020-10-27 07:56:47 +0100

    Enda flere tester

---
+++ src/data/store/YtelseSamendrag.tsx
@@ -0,0 +1,28 @@
+import React, { createContext, useState, useContext, Dispatch, SetStateAction } from 'react';
+// import YtelseSammendragTable from '../../components/YtelseSammendragTable';
+import { YtelseSammendrag } from '../../util/helseSpionTypes';
+
+interface YtelseSammendragProps {
+  children: React.ReactNode
+  ytelseSammendrag?: YtelseSammendrag[] | undefined
+}
+
+interface YtelsesammendragProps {
+  ytelsesammendrag: YtelseSammendrag[] | undefined,
+  setYtelsesammendrag: Dispatch<SetStateAction<YtelseSammendrag[] | undefined>>,
+}
+
+const YtelseSammendragContext = createContext<YtelsesammendragProps>({
+  ytelsesammendrag: [],
+  setYtelsesammendrag: () => {}
+});
+
+const YtelseSammendragProvider = (props: YtelseSammendragProps) => {
+  const [ ytelsesammendrag, setYtelsesammendrag ] = useState<YtelseSammendrag[] | undefined>(props.ytelseSammendrag);
+
+  return (<YtelseSammendragContext.Provider value={{ytelsesammendrag, setYtelsesammendrag}}>{ props.children }</YtelseSammendragContext.Provider>);
+};
+
+export default YtelseSammendragProvider;
+
+export const useYtelseSammendragContext = () => useContext(YtelseSammendragContext);
