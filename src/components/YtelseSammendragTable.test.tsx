58115c0aa9437d1a556a780ccec86bf8d7fa423d
Kent Fjellstad <kent.fjellstad@nav.no>
2020-10-27 13:55:24 +0100

    Sammkjøre mock data

--- src/components/YtelseSammendragTable.test.tsx
+++ src/components/YtelseSammendragTable.test.tsx
@@ -1,71 +1,76 @@
-import '@testing-library/jest-dom'
-import React from 'react'
-import { render, fireEvent, screen } from '@testing-library/react'
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { axe, toHaveNoViolations } from 'jest-axe';
-import StoreProvider from '../data/store/StoreProvider';
 
 import YtelseSammendragTable from './YtelseSammendragTable';
 import { YtelseSammendrag } from '../util/helseSpionTypes';
 
+import mockYtelsesammendrag from '../mockdata/mockYtelsesammendrag';
+import { testFnr } from '../mockdata/testFnr';
+
 expect.extend(toHaveNoViolations);
 
 describe('YtelseSammendragTable', () => {
-  const sammendrag: YtelseSammendrag[] = [
-    {
-      navn: 'Navn Navnesen',
-      identitetsnummer: '12345678901',
-      antall_refusjoner: 3,
-      merknad: 'merknad',
-      max_refusjon_dager: 3,
-      refusjonsbeløp: 123.4
-    },
-    {
-      navn: 'Test Testesen',
-      identitetsnummer: '12348878901',
-      antall_refusjoner: 2,
-      merknad: 'merknad 2',
-      max_refusjon_dager: 2,
-      refusjonsbeløp: 234.5
-    }
-  ];
+  const sammendrag: YtelseSammendrag[] = mockYtelsesammendrag;
 
   it('should render the component and display data', () => {
     const mockFunction = jest.fn();
 
-    render(<YtelseSammendragTable ytelseSammendrag={sammendrag} onNameClick={mockFunction} startdato="2020.01.01" sluttdato="2020.02.02"/>);
+    render(
+      <YtelseSammendragTable
+        ytelseSammendrag={sammendrag}
+        onNameClick={mockFunction}
+        startdato='2020.01.01'
+        sluttdato='2020.02.02'
+      />
+    );
 
-    expect(screen.getByText(/Test Testesen/)).toBeInTheDocument();
-    expect(screen.getByText(/12348878901/)).toBeInTheDocument();
+    expect(screen.getByText(/Donald Duck/)).toBeInTheDocument();
+    expect(screen.getByText(testFnr.GyldigeFraDolly.TestPerson2)).toBeInTheDocument();
     expect(screen.getByText(/merknad 2/)).toBeInTheDocument();
-    expect(screen.getByText(/234.5/)).toBeInTheDocument();
+    expect(screen.getByText(/234/)).toBeInTheDocument();
     expect(screen.getByText(/01.01.20/)).toBeInTheDocument();
     expect(screen.getByText(/02.02.20/)).toBeInTheDocument();
-    expect(screen.getByText(/357.9/)).toBeInTheDocument();
+    expect(screen.getByText(/942/)).toBeInTheDocument();
   });
 
   it('should render the component and display data', () => {
     const mockFunction = jest.fn();
 
-    render(<YtelseSammendragTable ytelseSammendrag={sammendrag} onNameClick={mockFunction} startdato="2020.01.01" sluttdato="2020.02.02"/>);
+    render(
+      <YtelseSammendragTable
+        ytelseSammendrag={sammendrag}
+        onNameClick={mockFunction}
+        startdato='2020.01.01'
+        sluttdato='2020.02.02'
+      />
+    );
 
-    const idNumberHeader = screen.getByText(/IDENTITY_NUMBER/)
+    const idNumberHeader = screen.getByText(/IDENTITY_NUMBER/);
 
     fireEvent.click(idNumberHeader);
 
-    expect(screen.getByText(/Test Testesen/)).toBeInTheDocument();
-    expect(screen.getByText(/12348878901/)).toBeInTheDocument();
-    expect(screen.getByText(/merknad 2/)).toBeInTheDocument();
-    expect(screen.getByText(/234.5/)).toBeInTheDocument();
+    expect(screen.getByText(/Donald Schneider/)).toBeInTheDocument();
+    expect(screen.getByText(testFnr.GyldigeFraDolly.TestPerson1)).toBeInTheDocument();
+    expect(screen.getByText(/Merknad på ytelse/)).toBeInTheDocument();
+    expect(screen.getByText(/234/)).toBeInTheDocument();
     expect(screen.getByText(/01.01.20/)).toBeInTheDocument();
     expect(screen.getByText(/02.02.20/)).toBeInTheDocument();
-    expect(screen.getByText(/357.9/)).toBeInTheDocument();
+    expect(screen.getByText(/942/)).toBeInTheDocument();
   });
 
   it('should have no a11y violations', async () => {
-
     const mockFunction = jest.fn();
 
-    const rendered = render(<YtelseSammendragTable ytelseSammendrag={sammendrag} onNameClick={mockFunction} startdato="2020.01.01" sluttdato="2020.02.02"/>);
+    const rendered = render(
+      <YtelseSammendragTable
+        ytelseSammendrag={sammendrag}
+        onNameClick={mockFunction}
+        startdato='2020.01.01'
+        sluttdato='2020.02.02'
+      />
+    );
 
     const results = await axe(rendered.container);
     expect(results).toHaveNoViolations();
--- src/mockdata/mockYtelsesammendrag.ts
+++ src/mockdata/mockYtelsesammendrag.ts
@@ -5,33 +5,33 @@ const ytelsesammendrag = [
     navn: 'Donald Schneider',
     identitetsnummer: testFnr.GyldigeFraDolly.TestPerson1,
     antall_refusjoner: 3,
-    merknad: '',
+    merknad: 'Merknad på ytelse',
     max_refusjon_dager: 2,
     refusjonsbeløp: 234
   },
   {
     navn: 'Donald Duck',
     identitetsnummer: testFnr.GyldigeFraDolly.TestPerson2,
-    antall_refusjoner: 3,
-    merknad: '',
+    antall_refusjoner: 4,
+    merknad: 'merknad 2',
     max_refusjon_dager: 2,
-    refusjonsbeløp: 234
+    refusjonsbeløp: 235
   },
   {
     navn: 'Donald Thump',
     identitetsnummer: testFnr.GyldigeFraDolly.TestPerson3,
-    antall_refusjoner: 3,
+    antall_refusjoner: 5,
     merknad: '',
     max_refusjon_dager: 2,
-    refusjonsbeløp: 234
+    refusjonsbeløp: 236
   },
   {
     navn: 'Donald von Schneider',
     identitetsnummer: testFnr.GyldigeFraDolly.TestPerson4,
-    antall_refusjoner: 3,
+    antall_refusjoner: 6,
     merknad: '',
     max_refusjon_dager: 2,
-    refusjonsbeløp: 234
+    refusjonsbeløp: 237
   }
 ];
 
