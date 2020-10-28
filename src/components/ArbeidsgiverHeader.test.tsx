0b84cc30a5546c7af32f582600d7e99ce10f4f0b
Kent Fjellstad <kent.fjellstad@nav.no>
2020-10-28 14:39:52 +0100

    La til test av arbeidsgiverheader

--- src/components/ArbeidsgiverHeader.test.tsx
+++ src/components/ArbeidsgiverHeader.test.tsx
@@ -6,47 +6,42 @@ import { axe, toHaveNoViolations } from 'jest-axe';
 import ArbeidsgiverHeader from './ArbeidsgiverHeader';
 import StoreProvider from '../data/store/StoreProvider';
 
-jest.mock('../data/useYtelseSammendrag');
-
-import useYtelseSammendrag from '../data/useYtelseSammendrag';
-import mockFetchedYtelsesperioder from '../mockdata/mockFetchedYtelsesperioder';
+import { testFnr } from '../mockdata/testFnr';
 
 jest.mock('../data/Ytelsesperioder');
 
 expect.extend(toHaveNoViolations);
 
-const mockHookFetch = jest.fn().mockResolvedValue(mockFetchedYtelsesperioder);
-
-useYtelseSammendrag.mockImplementation(() => {
-  return mockHookFetch;
-});
-
 describe('ArbeidsgiverHeader', () => {
-  // it('should render the component and display wait and then an error', async () => {
-  //   const fetchSpy = jest.spyOn(window, 'fetch');
-  //   const rendered = render(
-  //     <StoreProvider>
-  //       <ArbeidsgiverHeader
-  //         arbeidsgiverNavn='ArbeidsgiverNavn'
-  //         arbeidsgiverId='123'
-  //       />
-  //     </StoreProvider>
-  //   );
+  it('should render the component and display wait and then an error', async () => {
+    const fetchSpy = jest.spyOn(window, 'fetch');
+    const rendered = render(
+      <StoreProvider>
+        <ArbeidsgiverHeader
+          arbeidsgiverNavn='ArbeidsgiverNavn'
+          arbeidsgiverId='123'
+        />
+      </StoreProvider>
+    );
 
-  //   const fnrField = rendered.getByPlaceholderText('IDENTITY_NUMBER_EXT');
+    const fnrField = rendered.getByPlaceholderText('IDENTITY_NUMBER_EXT');
 
-  //   fireEvent.change(fnrField, {
-  //     target: { value: testFnr.GyldigeFraDolly.TestPerson1 }
-  //   });
+    fireEvent.change(fnrField, {
+      target: { value: testFnr.GyldigeFraDolly.TestPerson1 }
+    });
 
-  //   // const searchButton = rendered.getByText(/SEARCH/);
-  //   const searchButton = rendered.getByRole('button', { name: 'SEARCH' });
+    const searchButton = rendered.getByRole('button', { name: 'SEARCH' });
 
-  //   fireEvent.click(searchButton);
+    fireEvent.click(searchButton);
 
-  //   expect(rendered.getByText(/ArbeidsgiverNavn/)).toBeInTheDocument();
-  //   // expect(fetchSpy).toHaveBeenCalledWith({});
-  // });
+    expect(rendered.getByText(/ArbeidsgiverNavn/)).toBeInTheDocument();
+    expect(
+      fetchSpy
+    ).toHaveBeenCalledWith(
+      'http://localhost:3000/api/v1/ytelsesperioder/virksomhet/123?fom=2010-01-01&tom=2022-01-01',
+      { credentials: 'include', method: 'GET' }
+    );
+  });
 
   it('should have no a11y violations', async () => {
     const { container } = render(
