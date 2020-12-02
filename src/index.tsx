8633f9992dfba618ba39e404aee386bd6081a129
Morten Laukvik <laukvik@users.noreply.github.com>
2020-12-02 12:56:36 +0100

    Bruk sentry. Vis sonarbadges. Bruk riktig navn på sonar_token i workflow (#123)
    * Bruk sentry
    * Bruk standard navn på Sonar secret. Med underscore.
    * Bruk Sonar status badger på README.md

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import * as Sentry from '@sentry/browser';
+import env, {EnvironmentType} from './Environment';
+
+Sentry.init({
+  dsn: 'https://1fc3da75f22d4356adcfd1a7f7e841f2@sentry.gc.nav.no/49',
+  environment: EnvironmentType[env.environmentMode],
+});
 
 ReactDOM.render(
   <BrowserRouter basename="min-side-refusjon">
