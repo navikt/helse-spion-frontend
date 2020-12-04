6efc886cff0b94cf345a58683be2ae44eed97d9b
Morten Laukvik <morten.laukvik@nav.no>
2020-12-04 16:12:12 +0100

    Sonar eksludering

--- run-sonar.js
+++ run-sonar.js
@@ -9,7 +9,7 @@ scanner(
             'sonar.projectKey': 'navikt_helse-spion-frontend',
             'sonar.sources': 'src',
             'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
-            'sonar.coverage.exclusions': '**/*.test.t*,**/mock*.*'
+            'sonar.coverage.exclusions': '**/*.test.t*,**/mock*.*,setupTests.ts, src/index.tsx'
         }
     },
     () => process.exit()
