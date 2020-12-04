53ea4cb71fc286d335404418d215917470c0dada
Morten Laukvik <morten.laukvik@nav.no>
2020-12-04 16:05:43 +0100

    POH-489: Refaktorer til ei side

--- src/components/FnrSokeside.test.tsx
+++ src/components/FnrSokeside.test.tsx
@@ -1,26 +1,25 @@
 import '@testing-library/jest-dom'
 import React from 'react'
 import { render, fireEvent, screen } from '@testing-library/react'
-import { axe, toHaveNoViolations } from 'jest-axe';
+import { axe } from 'jest-axe';
 
 import FnrSokeside from './FnrSokeside';
 import StoreProvider from '../data/store/StoreProvider';
 import * as useYtelsesperioder from '../data/Ytelsesperioder';
 
-expect.extend(toHaveNoViolations);
-
-const mockHook = jest.fn();
-
-jest.spyOn(useYtelsesperioder,'default').mockImplementation(mockHook);
 
 describe('FnrSokeside', () => {
-  it('should render the component and display an info text', () => {
+  it('should render the component and display an info text2', () => {
+    const mockHook = jest.fn();
+    jest.spyOn(useYtelsesperioder,'default').mockImplementationOnce(mockHook);
     const rendered = render(<StoreProvider><FnrSokeside arbeidsgiverId="123456789"/></StoreProvider>);
 
     expect(rendered.getByText(/INFO_TEXT/)).toBeInTheDocument();
   })
 
   it('should render the component and display an info text', () => {
+    const mockHook = jest.fn();
+    jest.spyOn(useYtelsesperioder,'default').mockImplementationOnce(mockHook);
     const rendered = render(<StoreProvider><FnrSokeside arbeidsgiverId="123456789"/></StoreProvider>);
 
     const inputField = screen.getByLabelText('IDENTITY_NUMBER_EXT');
