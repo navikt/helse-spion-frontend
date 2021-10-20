0029b0841d8226aba822b8a03af1b4511968cf7d
Kent Fjellstad <kent.fjellstad@nav.no>
2021-10-20 13:15:28 +0200

    Fikser tester som feiler

--- src/util/validateDato.ts
+++ src/util/validateDato.ts
@@ -2,7 +2,7 @@ import dayjs from 'dayjs';
 
 const validateDato = (fom: string): string | undefined => {
   if (!dayjs(fom).isValid()) {
-    return 'Må være gyldige dato';
+    return 'Må være gyldig dato';
   }
   return undefined;
 };
