99e249abc1ff176a641e8853da8bae2d2d2101b7
Kent Fjellstad <kent.fjellstad@nav.no>
2021-11-10 16:01:37 +0100

    Lint er ikke helt fornøyd

--- src/components/PersonOversiktTabell.test.tsx
+++ src/components/PersonOversiktTabell.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 import { axe } from 'jest-axe';
 import PersonOversiktTabell from './PersonOversiktTabell';
--- src/components/Personoppslag.test.tsx
+++ src/components/Personoppslag.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 import { axe } from 'jest-axe';
 import StoreProvider from '../data/store/StoreProvider';
