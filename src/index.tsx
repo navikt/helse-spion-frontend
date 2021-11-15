4c672b2b4bd561b7cd581143be4e09c0bc4acfee
Kent Fjellstad <kent.fjellstad@nav.no>
2021-11-15 08:49:01 +0100

    Sentry var installert feil

--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,21 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import * as Sentry from '@sentry/browser';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import env, { EnvironmentType } from './Environment';
 
 if (env.environmentMode !== EnvironmentType.LOCAL) {
   Sentry.init({
     dsn: 'https://1fc3da75f22d4356adcfd1a7f7e841f2@sentry.gc.nav.no/49',
     environment: EnvironmentType[env.environmentMode],
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 0.5
   });
 }
 
 ReactDOM.render(
-  <BrowserRouter basename="min-side-refusjon">
+  <BrowserRouter basename='min-side-refusjon'>
     <App />
   </BrowserRouter>,
   document.getElementById('root') as HTMLElement
