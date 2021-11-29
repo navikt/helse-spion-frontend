fe0806a0005025107ee2c3930ece3eccb2ae8792
Kent Fjellstad <kent.fjellstad@nav.no>
2021-11-29 14:11:59 +0100

    Utkommentert kode

--- src/components/ArbeidsgiverHeader.tsx
+++ src/components/ArbeidsgiverHeader.tsx
@@ -84,13 +84,6 @@ const ArbeidsgiverHeader = ({
           </Innholdstittel>
         </Column>
       </Row>
-      {/* <Row className='arbeidsgiver-periode--lufting'>
-        <Column sm='12'>
-          <Lenke className='arbeidsgiver-periode--lufting' href=''>
-            &lt;&lt; {t(Keys.ALL_REFUNDS)}
-          </Lenke>
-        </Column>
-      </Row> */}
       <Row className='arbeidsgiver-periode--lufting'>
         <Column sm='6' className='ytelsesperiode-datovelger'>
           <label>
@@ -102,9 +95,7 @@ const ArbeidsgiverHeader = ({
                 id='fom_datovelger'
                 dato={dayjs(fraDato).toDate()}
                 feilmelding={fraError}
-                // label='Fra dato'
                 placeholder='dd.mm.yyyy'
-                // disabled={item.accepted}
                 minDate={min}
                 maxDate={max}
                 className='datovelger datovelger-fra'
@@ -115,9 +106,7 @@ const ArbeidsgiverHeader = ({
                 id='tom_datovelger'
                 dato={dayjs(tilDato).toDate()}
                 feilmelding={tilError}
-                // label='Til dato'
                 placeholder='dd.mm.yyyy'
-                // disabled={item.accepted}
                 minDate={min}
                 maxDate={max}
                 className='datovelger datovelger-til'
